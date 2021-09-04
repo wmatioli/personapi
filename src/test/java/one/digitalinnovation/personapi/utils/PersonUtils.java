afbf0541721f1e30e105fc89a797658c8662a3de
wmatioli <wagner.matioli@gmail.com>
2021-09-04 18:42:10 -0300

    Cadastro alterado

--- src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -8,11 +8,11 @@ import java.util.Collections;
 
 public class PersonUtils {
 
-    private static final String FIRST_NAME = "Rodrigo";
-    private static final String LAST_NAME = "Peleias";
-    private static final String CPF_NUMBER = "369.333.878-79";
+    private static final String FIRST_NAME = "Wagner";
+    private static final String LAST_NAME = "Matioli";
+    private static final String CPF_NUMBER = "253.041.978-79";
     private static final long PERSON_ID = 1L;
-    public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
+    public static final LocalDate BIRTH_DATE = LocalDate.of(1975, 01, 11);
 
     public static PersonDTO createFakeDTO() {
         return PersonDTO.builder()
